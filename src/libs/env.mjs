73daee45bf8f800c2b805a442dccfcde039d759f
Thomas_DevArk <github.fggvs@slmail.me>
2023-05-27 15:28:28 +0100

    Added NEXTAUTH_SECRET

--- src/libs/env.mjs
+++ src/libs/env.mjs
@@ -2,13 +2,16 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 export const env = createEnv({
-  server: {
-    NEXT_AUTH_GOOGLE_ID: z.string().min(1),
-    NEXT_AUTH_GOOGLE_SECRET: z.string().min(1),
-  },
-  client: {},
-  runtimeEnv: {
-    NEXT_AUTH_GOOGLE_ID: process.env.NEXT_AUTH_GOOGLE_ID,
-    NEXT_AUTH_GOOGLE_SECRET: process.env.NEXT_AUTH_GOOGLE_SECRET,
-  },
+    server: {
+        NEXT_AUTH_GOOGLE_ID: z.string().min(1),
+        NEXT_AUTH_GOOGLE_SECRET: z.string().min(1),
+        NEXTAUTH_SECRET: z.string().min(1),
+    },
+    client: {},
+    runtimeEnv: {
+        NEXT_AUTH_GOOGLE_ID: process.env.NEXT_AUTH_GOOGLE_ID,
+        NEXT_AUTH_GOOGLE_SECRET: process.env.NEXT_AUTH_GOOGLE_SECRET,
+
+        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    },
 });
